339c41273758a819b17cc6ca5623f5f07487ce8a
MNET_01 <MNET_01@MNET>
2026-04-07 12:02:39 +0900

    git push

---
+++ jenkins/src/main/java/jekinsTemp/jenkins/App.java
@@ -0,0 +1,13 @@
+package jekinsTemp.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jenkins!" );
+    }
+}
---
+++ jenkins/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" %>
+<html>
+<body>
+<h1>Hello WAS + Jenkins</h1>
+</body>
+</html> 
